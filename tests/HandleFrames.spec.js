b231a940183b37bd82b9a34246b828e2d238e723
Esports King <divpadsmitais@gmail.com>
2026-05-13 20:46:50 +0300

    added nested frame handling

--- tests/HandleFrames.spec.js
+++ tests/HandleFrames.spec.js
@@ -19,3 +19,16 @@ test('Handling Frames', async ({page}) => {
     await frameLocator.fill('Welcome to Frame 2');
     await page.waitForTimeout(3000);
 })
+
+test.only('Handling Nested Frames', async ({page}) => {
+
+    await page.goto('https://ui.vision/demo/webtest/frames/');
+    const frame3 = await page.frame({url: 'https://ui.vision/demo/webtest/frames/frame_3.html'});
+    await frame3.fill("[name='mytext3']", 'Welcome to Frame 3');
+
+    //nested frame
+    const childFrames = await frame3.childFrames();
+    console.log('Total Child Frames: ' + childFrames.length);
+    await childFrames[0].locator("//*[@id='i6']/div[3]/div").check();
+    await page.waitForTimeout(3000);
+})
